2dc894ffaae7712adaa1272f3947123c86c2a52d
Devang Gaur <dgaur@redhat.com>
2018-02-02 02:41:17 +0530

    Added route parameterization

--- src/api/routes.js
+++ src/api/routes.js
@@ -1,7 +1,7 @@
 var req_handlers = require("./handlers.js").request_handlers()
 
-var appRouter = function(app) {
-	app.post("/api/v1.0/measurements", function(req , res){
+var appRouter = function(app, route_prefix) {
+	app.post(route_prefix + "/measurements", function(req , res){
 		console.log("The request body is: ", req.body)
 		req_handlers.handle_stats_ingestion(req.body).then(function (result) {
 			res.status(200)
@@ -13,12 +13,12 @@ var appRouter = function(app) {
 		})
 	}),
 
-	app.get("/api/v1.0/readiness", function(req , res){
+	app.get(route_prefix + "/readiness", function(req , res){
 		res.status(200)
 		res.end()
 	}),
 
-	app.get("/api/v1.0/liveness", function(req , res){
+	app.get(route_prefix + "/liveness", function(req , res){
 		res.status(200)
 		res.end()
 	})
